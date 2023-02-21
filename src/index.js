26ba7331d46c7ca647ae7eaeda2063cfa3d8d81d
Garvit Joshi <joshi.garvit22@gmail.com>
2023-02-21 09:01:39 +0530

    Added router to the website

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './stylesheets/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import PersonalPage from './routes/PersonalPage/PersonalPage';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />}></Route>
+        <Route path="about/developers/GarvitJoshi" element={<PersonalPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <PersonalPage />
-  </React.StrictMode>
-);
+root.render(<Router />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
